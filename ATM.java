910cf0f45003fef5fabb9c3f8ca7e771c5edf0e4
LeirbagSouls <gabriel.prztz2003@gmail.com>
2024-06-17 01:37:31 -0600

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package atm;
 
-/**
- *
- * @author isabr
- */
 import java.text.DecimalFormat;
 import java.util.InputMismatchException;
 import java.util.Scanner;
